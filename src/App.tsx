6f053cde1f3105f56b6abe8a5ac5f37251b2ee92
Eduard Bardají Puig <edu@trustmachines.co>
2023-01-28 00:54:14 +0000

    feat: add faucet request

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Navigate, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Button, Container, Flex, Text, Group, MantineProvider } from '@mantine/core';
+import { Button, Container, Flex, Text, Group, MantineProvider, Menu, Loader } from '@mantine/core';
 import { StackingClientProvider } from '@components/stacking-client-provider/stacking-client-provider';
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
 import { loadFonts } from '@utils/load-fonts';
@@ -20,6 +20,7 @@ import {
   useBlockchainApiClient,
 } from '@components/blockchain-api-client-provider';
 import { StartDirectStacking } from './pages/stacking/start-direct-stacking/start-direct-stacking';
+import { IconDroplet } from '@tabler/icons-react';
 
 function Profile() {
   const { address } = useAuth();
@@ -54,29 +55,70 @@ function Profile() {
 }
 
 function Layout() {
+  const { address } = useAuth();
   const { isSignedIn, signOut } = useAuth();
   const { networkName, setNetworkByName } = useNetwork();
+  const { faucetsApi } = useBlockchainApiClient();
 
+  if (!address) {
+    // TODO: log error
+    const id = '42d06620-f87d-487e-8f54-cb171b3e3ea6';
+    const msg = 'Expected `address` to be defined.';
+    console.error(id, msg);
+    return <ErrorAlert id={id}>{msg}</ErrorAlert>;
+  }
+
+  const networkButton = (
+    <Button
+      variant="outline"
+      onClick={() => {
+        if (networkName === 'mainnet') {
+          setNetworkByName('testnet');
+          return;
+        }
+
+        setNetworkByName('mainnet');
+      }}
+    >
+      {networkName !== 'mainnet' ? '🚧 ' : ''}
+      {networkName}
+    </Button>
+  );
   return (
     <>
       <Flex h="100vh" direction="column">
         {isSignedIn && (
           <Group p="sm" position="right">
             <Profile />
-            <Button
-              variant="outline"
-              onClick={() => {
-                if (networkName === 'mainnet') {
-                  setNetworkByName('testnet');
-                  return;
-                }
-
-                setNetworkByName('mainnet');
-              }}
-            >
-              {networkName !== 'mainnet' ? '🚧 ' : ''}
-              {networkName}
-            </Button>
+            {networkName === 'testnet' ? (
+              <Menu trigger="hover">
+                <Menu.Target>{networkButton}</Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item
+                    onClick={async () => {
+                      try {
+                        // alert requesting
+                        await faucetsApi.runFaucetStx({
+                          address,
+                          stacking: true,
+                        });
+                        // alert success
+                        // enable
+                      } catch (e) {
+                        // alert error
+                        // enable
+                      }
+                    }}
+                    icon={<IconDroplet size={14} />}
+                    // icon={<Loader size={14} />}
+                  >
+                    <Text>Request STX from Faucet</Text>
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            ) : (
+              networkButton
+            )}
             <Button onClick={() => signOut()}>Sign out</Button>
           </Group>
         )}
