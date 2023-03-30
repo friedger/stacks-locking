30d34ae7be5780dccd1077021ad21dc770ff2c5c
friedger <mail@friedger.de>
2023-03-30 18:23:24 +0200

    fix: remove unused code

--- src/utils/stacking.ts
+++ src/utils/stacking.ts
@@ -1,24 +1,6 @@
-import { sha256 } from '@noble/hashes/sha256';
-import { base58check } from '@scure/base';
 import { poxAddressToBtcAddress } from '@stacks/stacking';
-import { AddressHashMode } from '@stacks/transactions';
-import BN from 'bn.js';
-import { Buffer } from 'buffer';
 
 import { NETWORK } from '@constants/app';
-
-// TODO: Can't figure out the right types
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const poxKeyToVersionBytesMap: Record<'mainnet' | 'testnet', any> = {
-  mainnet: {
-    [AddressHashMode.SerializeP2PKH]: 0x00,
-    [AddressHashMode.SerializeP2SH]: 0x05,
-  },
-  testnet: {
-    [AddressHashMode.SerializeP2PKH]: 0x6f,
-    [AddressHashMode.SerializeP2SH]: 0xc4,
-  },
-};
 
 interface ConvertToPoxAddressBtc {
   version: Uint8Array;
